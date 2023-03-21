5cb8656d6c36c03e6b0f6e7c2107566cb1f25637
Neelesh2909 <rawat.neelesh9@gmail.com>
2023-03-21 09:50:12 +0530

    [Neelesh] Add: shell program for demo dict

---
+++ Day 5/dict.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -x
+
+declare -A sounds 
+sounds[dog]="bark" 
+sounds[cow]="moo" 
+sounds[bird]="tweet" 
+sounds[wolf]="howl"
+
+echo ${sounds[dog]}
+echo ${sounds[@]}
+echo ${!sounds[@]}
+echo ${#sounds[@]}
+
+unset sounds[dog]
+
+echo ${sounds[dog]}
